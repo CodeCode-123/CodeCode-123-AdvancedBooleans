14471dc6907f4c34400474dd38e22ba292f09f4e
Shen Tang <adatang23@yahoo.com>
2025-06-03 12:07:04 +0000

    completed

--- src/main/java/AdvancedBooleanLogic.java
+++ src/main/java/AdvancedBooleanLogic.java
@@ -15,9 +15,7 @@ public class AdvancedBooleanLogic {
      * @param b2 second boolean to be compared.
      * @return false if b1 and b2 are both true. Otherwise, return true.
      */
-    public boolean returnNotBoth(boolean b1, boolean b2){ 
-        return !(b1 && b2); 
-    }
+    public boolean returnNotBoth(boolean b1, boolean b2){ return !(b1 && b2); }
 
     /**
      * b1 OR b2, BUT NOT BOTH
@@ -25,9 +23,7 @@ public class AdvancedBooleanLogic {
      * @param b2 second boolean to be compared.
      * @return true if either b1 or b2 is true, but false if they are both true or both false.
      */
-    public boolean returnExclusiveOr(boolean b1, boolean b2){ 
-        return b1 != b2; 
-    }
+    public boolean returnExclusiveOr(boolean b1, boolean b2){ return b1 != b2; }
 
     /**
      * Neither b1 NOR b2
@@ -35,7 +31,5 @@ public class AdvancedBooleanLogic {
      * @param b2 second boolean to be compared.
      * @return true if neither b1 nor b2 are true. Return false if either b1 or b2 is true.
      */
-    public boolean returnNeitherNor(boolean b1, boolean b2){ 
-        return !(b1 || b2); 
-    }
+    public boolean returnNeitherNor(boolean b1, boolean b2){ return !(b1 || b2); }
 }
