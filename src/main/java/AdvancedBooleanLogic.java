7ed669b69e2dde0bba805fdcbf423f1ae233aecd
Shen Tang <adatang23@yahoo.com>
2025-06-03 11:57:44 +0000

    completed the Lab

--- src/main/java/AdvancedBooleanLogic.java
+++ src/main/java/AdvancedBooleanLogic.java
@@ -22,9 +22,7 @@ public class AdvancedBooleanLogic {
      * @param b2 second boolean to be compared.
      * @return true if either b1 or b2 is true, but false if they are both true or both false.
      */
-    public boolean returnExclusiveOr(boolean b1, boolean b2){
-        return b1 != b2;
-    }
+    public boolean returnExclusiveOr(boolean b1, boolean b2){ return b1 != b2; }
 
     /**
      * Neither b1 NOR b2
